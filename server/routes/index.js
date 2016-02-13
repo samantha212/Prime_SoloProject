c124a903ed86e3b3c023eb4ba9ce5d42b8259b68
Samantha Musselman <samantha.musselman212@gmail.com>
2016-02-13 08:59:58 -0600

    Organizes index.js page.

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,45 +11,25 @@ var connectionString = 'postgres://localhost:5432/song_shaker';
 
 pg.defaults.poolsize = 30;
 
-//Find some way to store this.
-function loggedIn(request, response, next) {
-    console.log("Logged in function being checked.");
-    if (request.user) {
-        next();
-    } else {
-        response.sendFile(path.join(__dirname, '../public/views/login.html'));
-    }
-}
-
-router.get('/printable_set', function(request, response){
-    response.sendFile(path.join(__dirname, '../public/views/printable_set.html'));
+router.get('/login', function(request, response){
+    response.sendFile(path.join(__dirname, '../public/views/login.html'));
 });
 
-//Success and failure redirects for user registration.
-router.get('/home', loggedIn, function(request, response){
-    console.log('Successful login for', request.user);
-    response.sendFile(path.join(__dirname, '../public/views/home.html'));
+router.get('/', function(request, response){
+    response.sendFile(path.join(__dirname, '../public/views/login.html'));
 });
 
 router.get('/fail', function(request, response){
     response.sendFile(path.join(__dirname, '../public/views/fail.html'));
 });
 
-router.get('/login', function(request, response){
-    response.sendFile(path.join(__dirname, '../public/views/login.html'));
+router.get('/home', loggedIn, function(request, response){
+    console.log('Successful login for', request.user);
+    response.sendFile(path.join(__dirname, '../public/views/home.html'));
 });
-
-
-
-
-
-
-
 
 router.get('/logout', function(request, response){
     request.logout();
-    console.log("logged out", request.user);
-
     response.sendFile(path.join(__dirname, '../public/views/login.html'));
 });
 
@@ -58,10 +38,14 @@ router.post('/', passport.authenticate('local', {
     failureRedirect: '/fail'
 }));
 
-router.get('/', function(request, response){
-    console.log("/ route being hit");
-    response.sendFile(path.join(__dirname, '../public/views/login.html'));
-});
 
+function loggedIn(request, response, next) {
+    console.log("Logged in function being checked.");
+    if (request.user) {
+        next();
+    } else {
+        response.sendFile(path.join(__dirname, '../public/views/login.html'));
+    }
+}
 
 module.exports = router;
