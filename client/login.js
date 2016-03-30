94fef44b2a80ce8df43c6225614c293da543ddd1
Samantha Musselman <samantha.musselman212@gmail.com>
2016-03-30 18:10:17 -0500

    Clean up formatting.

--- client/login.js
+++ client/login.js
@@ -5,12 +5,14 @@ app.controller('ButtonController', ['$scope', function($scope){
     $scope.goToRegistration = function () {
         var currentLocation = location.origin;
         var newLocation = currentLocation + "/registration";
+
         window.location.assign(newLocation);
     };
 
     $scope.goToLogIn = function() {
         var currentLocation = location.origin;
         var newLocation = currentLocation + "/login";
+
         window.location.assign(newLocation);
     };
 
